6db1f14ef544284ca6635979e0520ff323612fbc
KrzysztofAktas <krzysztofaktas@hotmail.com>
2024-04-23 14:47:04 +0100

    on slide 23. Added shopping cart cartItem and starting to filter cheeses.

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -1,7 +1,40 @@
-﻿namespace MyCheeseShop.Model
+﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using MyCheeseShop.Context;
+using System.Collections;
+using System.ComponentModel;
+using System.Threading.Tasks.Dataflow;
+
+
+namespace MyCheeseShop.Model
 {
     public class ShoppingCart
     {
-        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public event Action? OnCartUpdated;
+        private List<CartItem> _items;
+
+        public ShoppingCart()
+        {
+            _items = [];
+        }
+
+        public void AddItem(Cheese cheese, int quantity)
+        {
+            var item = _items.FirstOrDefault(item => ITempDataDictionary.Cheese.Id == cheese.Id);
+            if (item == null)
+            {
+                _items.Add(new CartItem { Cheese = cheese, Quantity = quantity });
+            }
+            else
+                item.Quantity += quantity;
+
+            OnCartUpdated?.Invoke();
+        }
+        public IEnumerable<CartItem> GetItems()
+        {
+            return _items;
+        }
     }
 }
