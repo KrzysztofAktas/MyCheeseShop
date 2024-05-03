abc0535dffef9b81fcb6e7cb267ae409ae11a0ed
KrzysztofAktas <krzysztofaktas@hotmail.com>
2024-05-03 10:33:24 +0100

    Filtered cheeses. new pages have been added: checkout, cart, orders. (dont work)

--- MyCheeseShop/Model/OrderItem.cs
+++ MyCheeseShop/Model/OrderItem.cs
@@ -6,6 +6,7 @@
         public Order Order { get; set; }
         public Cheese Cheese { get; set; }
         public int Quantity { get; set; }
+        public decimal Total => Cheese.Price * Quantity;
 
 
     }
